58cd84f9defb5cc6a43c4ecac45b4321f3da2fc7
roman.gaponov <roman.gaponov@payallps.com>
2024-06-29 18:50:04 +0300

    virtual app is done

--- virtual_threads/src/main/java/vthreads/controller/ThreadController.java
+++ virtual_threads/src/main/java/vthreads/controller/ThreadController.java
@@ -10,10 +10,7 @@ import java.util.logging.Logger;
 @RequestMapping("/thread")
 public class ThreadController {
     private static final Logger logger = Logger.getLogger(ThreadController.class.getName());
-    @GetMapping("/name")
-    public String getThreadName() {
-        return Thread.currentThread().toString();
-    }
+
     @GetMapping("/work")
     public String hard() throws InterruptedException {
         logger.info("working hard");
